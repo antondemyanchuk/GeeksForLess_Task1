627cf139fc580d62c4f69c1eee80ee4be743ba5c
Anton Demyanchuk <anton.demyanchuk@gmail.com>
2023-09-22 03:04:53 +0300

    NOT WORKING
    Beginning to add new ser\deser features

---
+++ Task1/Controllers/CatalogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml;
+using Task1.DAO;
+
+namespace Task1.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly SampleContext _context;
+
+        public CatalogController(SampleContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult SaveCatalogToJson()
+        {
+            var catalogStructure = _context.Cataloges;
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            };
+            var json = JsonConvert.SerializeObject(catalogStructure, jsonSettings);
+
+            System.IO.File.WriteAllText("catalog.json", json);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult LoadCatalogFromJson()
+        {
+            var json = System.IO.File.ReadAllText("catalog.json");
+
+            var catalogStructure = JsonConvert.DeserializeObject<List<Catalog>>(json);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
